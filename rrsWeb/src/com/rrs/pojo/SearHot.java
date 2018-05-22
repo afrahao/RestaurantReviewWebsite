2b76888288e878ae4ba3be410e2eaecd63cebaaf
Xieyutong <whisperxyt@163.com>
2018-05-22 10:47:10 +0800

    1. 完成按照名字、城市、地址、标签搜索 2. 完成历史搜索、热门搜索

---
+++ rrsWeb/src/com/rrs/pojo/SearHot.java
@@ -0,0 +1,30 @@
+package com.rrs.pojo;
+
+public class SearHot {
+	
+	int id;
+	String hotkey;
+	int count;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getHotkey() {
+		return hotkey;
+	}
+	public void setHotkey(String hotkey) {
+		this.hotkey = hotkey;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+
+}
